6ac5ca26024024d359a3d2811ff700cf0c1d1a74
Mukunda-Rijyal <mukundarijyal@gmail.com>
2026-03-31 11:02:28 +0545

    ind the factorial of a given integer using recursion

---
+++ Lab_3/fourteen.c
@@ -0,0 +1,17 @@
+/*14. Write a program in C to find the factorial of a given integer using recursion.*/
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n-1);
+}
+
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("Factorial of %d is %d\n", num, factorial(num));
+    return 0;
+}
